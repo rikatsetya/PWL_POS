6d2e0cbfcaeec8f80eb0183174a81ebd60e921f2
rikatsetya <rikatsetyagus@gmail.com>
2024-09-23 09:00:20 +0700

    "Commit JS 4 Praktikum 1"

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,17 +12,17 @@ class UserController extends Controller
     {
 
         // tambah data user dengan Eloquent Model
-        // $data = [
-        //     'username' => 'customer-1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 5
-        // ];
-        // UserModel::insert($data); // tambahkan data ke tabel m_user
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
-        UserModel::where('username', 'customer-1')->update($data); // update data user
+        UserModel::create($data); // tambahkan data ke tabel m_user
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama',
+        // ];
+        // UserModel::where('username', 'customer-1')->update($data); // update data user
 
         // coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_user
