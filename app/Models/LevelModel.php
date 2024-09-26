26b1698b3f90a247ad14f81b9a180a3a55f76518
rikatsetya <rikatsetyagus@gmail.com>
2024-09-26 14:16:15 +0700

    "Commit JS 5 Praktikum 3"

--- app/Models/LevelModel.php
+++ app/Models/LevelModel.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class LevelModel extends Model
 {
@@ -13,8 +14,8 @@ class LevelModel extends Model
     protected $table = 'm_level'; // Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'level_id'; // Mendefinisikan primary key dari tabel yang digunakan
 
-    public function user():BelongsTo
+    public function user():HasMany
     {
-        return $this->belongsTo(UserModel::class, 'user_id', 'user_id');
+        return $this->hasMany(UserModel::class, 'user_id', 'user_id');
     }
 }
