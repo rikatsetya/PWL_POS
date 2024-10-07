ded67215e764d3ba7ab9eb5f2897d9cb0e79c9c4
rikatsetya <rikatsetyagus@gmail.com>
2024-10-07 23:14:18 +0700

    "Commit JS 7 Tugas 4 Register"

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,9 @@ Route::pattern('id', '[0-9]+');
 Route::get('login', [AuthController::class, 'login'])->name('login');
 Route::post('login', [AuthController::class, 'postlogin']);
 Route::get('logout', [AuthController::class, 'logout'])->middleware('auth');
+Route::get('register', [AuthController::class, 'register']);
+Route::post('register', [AuthController::class, 'store']);
+
 
 Route::middleware(['auth'])->group(function () {
 
