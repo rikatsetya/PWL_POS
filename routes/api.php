4ec53da17a81c312c94afd66ac8c461bf3fd4a00
rikatsetya <rikatsetyagus@gmail.com>
2024-10-28 10:39:28 +0700

    "Commit JS 10 Praktikum 3"

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('lo
 Route::middleware('auth:api')->get('/user', function (Request $request){
     return $request->user();
 });
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
