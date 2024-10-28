0d56a7fd7d211497808f10fea20d5d11199650f1
rikatsetya <rikatsetyagus@gmail.com>
2024-10-28 11:13:13 +0700

    "Commit JS 10 Praktikum 4"

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\LevelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,6 +22,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request){
 });
 Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::get('levels', [LevelController::class, 'index']);
+Route::post('levels', [LevelController::class, 'store']);
+Route::get('levels/{level}', [LevelController::class, 'show']);
+Route::put('levels/{level}', [LevelController::class, 'update']);
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);
